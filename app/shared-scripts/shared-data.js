d58dcdaffb5313af41fae435421152247590f6d5
landsquid9 <daniel.bandfield@ntlworld.com>
2017-04-18 19:12:38 +0100

    message

--- app/shared-scripts/shared-data.js
+++ app/shared-scripts/shared-data.js
@@ -1,10 +1,10 @@
 /* Potential Server Addresses */
-var _webUrl = 'ws://connectivity-92668.onmodulus.net/';
+var _webUrl = 'ws://aether-iot.herokuapp.com/';
 var _localUrl = 'ws://localhost:3000';
 var _lanUrl = 'ws://192.168.2.104:3000';
 
 /* Change this to choose server */
-var url = _localUrl;
+var url = _webUrl;
 
 /* Export desired address */
 module.exports = url;
